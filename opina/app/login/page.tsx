cf988d3315b1855c40e33f3ab7c3dde68068f5f1
Guilherme A. Fortes <guilhermefortesoffice@gmail.com>
2026-07-22 19:52:03 -0300

    feat(ui): implementar estrutura inicial da interface de autenticação
    - configura a estrutura inicial do projeto utilizando Next.js.
    - desenvolve layout responsivo da tela de login com Tailwind CSS.
    - adiciona identidade visual da aplicação Opina.
    - insere logotipo na área institucional da tela.
    - implementa seção de apresentação da plataforma.
    - cria formulário inicial de autenticação.
    - adiciona campos de entrada para e-mail e senha.
    - inclui ícones ilustrativos nos campos utilizando Lucide React.
    - implementa botão principal de acesso.
    - adiciona link para recuperação de senha.
    - adiciona link para tela de cadastro.
    - implementa separador visual para métodos alternativos de login.
    - adiciona botões de login social com ícones do Google, LinkedIn e Facebook.
    - aplica estilização inicial dos componentes utilizando Tailwind CSS.
    - organiza a estrutura visual utilizando Flexbox e Grid.
    - adiciona efeitos de foco, hover e sombras para melhorar a experiência do usuário.
    
    Obs.: Este commit contempla apenas a construção da interface.

---
+++ opina/app/login/page.tsx
@@ -0,0 +1,91 @@
+import { Mail, Lock } from "lucide-react";
+import { FaGoogle, FaLinkedinIn, FaFacebookF} from "react-icons/fa";
+
+export default function Login() {
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-2 min-h-screen">
+			<div className="flex flex-col justify-center items-center bg-blue-100 p-8">
+				<div className="flex flex-col items-center">
+					<div className="w-28 h-28 rounded-full border-4 border-blue-500 overflow-hidden">
+							<img
+									src="/opina.png"
+									alt="Logo"
+									className="w-full h-full object-cover"
+							/>
+					</div>
+					<p className="mt-5 text-lg">Crie enquetes em segundos e veja as opiniões em tempo real.</p>
+				</div>
+			</div>
+			<div className="flex flex-col justify-center items-center bg-gray-100 p-8">
+				<form className="flex flex-col justify-center items-center h-full p-8 bg-white w-100 rounded-2xl shadow-md" >
+					<h1 className="text-3xl font-extrabold tracking-tight text-blue-500 mb-2">Bem-vindo de volta!</h1>
+					<div className="flex flex-col gap-4 mt-4 w-full max-w-sm">
+						<div className="relative">
+							<Mail
+								className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"
+								size={20}
+							/>
+							<input
+								type="email"
+								placeholder="E-mail"
+								className="w-full pl-10 pr-4 py-1.5 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
+							/>
+						</div>
+
+						<div className="relative">
+							<Lock
+								className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"
+								size={20}
+							/>
+							<input
+								type="password"
+								placeholder="Password"
+								className="w-full pl-10 pr-4 py-1.5 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
+							/>
+						</div>
+					</div>
+					<div className="flex justify-end w-full max-w-sm mt-2">
+						<a href="/forgot-password" className="text-sm text-blue-500 hover:underline">Esqueceu a senha?</a>
+					</div>
+					<div className="flex flex-col gap-4 mt-4 w-full max-w-sm">
+						<button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">Entrar</button>
+					</div>
+					
+					<div className="flex justify-center w-full max-w-sm mt-2">
+						<p className="text-sm text-gray-600 text-center">
+								Não tem uma conta? <a href="/register" className="text-blue-500 hover:underline">Cadastre-se</a>
+						</p>
+					</div>
+					<div className="flex items-center my-6 w-full max-w-sm">
+						<div className="flex-1 border-t border-gray-300"></div>
+						<span className="px-4 text-sm text-gray-500">ou</span>
+						<div className="flex-1 border-t border-gray-300"></div>
+					</div>
+
+					<div className="flex justify-center gap-4">
+						<button
+							type="button"
+							className="w-9 h-9 rounded-full border border-gray-300 flex items-center justify-center hover:bg-gray-100 transition"
+						>
+							<FaGoogle className="text-red-500 text-xl" size={20}/>
+						</button>
+
+						<button
+							type="button"
+							className="w-9 h-9 rounded-full border border-gray-300 flex items-center justify-center hover:bg-gray-100 transition"
+						>
+							<FaLinkedinIn className="text-blue-700 text-xl" size={20}/>
+						</button>
+
+						<button
+							type="button"
+							className="w-9 h-9 rounded-full border border-gray-300 flex items-center justify-center hover:bg-gray-100 transition"
+						>
+							<FaFacebookF className="text-blue-600 text-xl" size={20}/>
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+}
